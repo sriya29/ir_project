07cf719483267dbf85c424a72b39c65723aede26
sriya29 <sriya.muppidi@gmail.com>
2017-09-21 15:11:45 +0530

    Update invert.py

--- invert.py
+++ invert.py
@@ -3,6 +3,7 @@ from nltk.corpus import stopwords
 import os,re
 import porter
 import nltk.tokenize.api
+import json,shelve
 def normalize(word):
 	regex=re.compile('[^a-zA-Z0-9]')
 	word=regex.sub("",word)
@@ -17,37 +18,31 @@ list=""
 list_t=[]
 final_set=set(list)
 final_dict={}
+list_words_dict={}
 for filename in os.listdir("."):
 	file=open(filename,"rb")
 	stri=file.read()
-	list_words=(str(stri).split(" "))
-	for i in range(0,len(list_words)):
-		list_words[i]=normalize(list_words[i])
-	list_set=set(list_words)
+	list_words_dict[filename]=str(stri).split(" ")
+	for i in range(0,len(list_words_dict[filename])):
+		list_words_dict[filename][i]=normalize(list_words_dict[filename][i])
+	list_set=set(list_words_dict[filename])
 	final_set=final_set|list_set
 	#final_set.remove("")
+k=0
+
 for word in final_set:
 	final_dict[word]={}
 	list_dict={}
-	if(word):
-		for filename in os.listdir("."):
-			file=open(filename,"rb")
-			stri=file.read()
-			list_words=(str(stri).split(" "))
-			for i in range(0,len(list_words)):
-				list_words[i]=normalize(list_words[i])								
-			if word in list_words:			#			#print(list_tuple)
-				#list_dict[filename]=list_words.count(word)
-				#final_dict[word].append(list_dict)
-				final_dict[word][filename]=list_words.count(word)
-#			#print(final_dict[word][filename])
-		#break
-#print(list_t)
-#	break		
-strin=str(final_dict)
-fp=open("fg.txt","w+")
-fp.write(strin)			
+	if(word!=""):
+		for key,value in list_words_dict.items():
+			if word in value:
+				final_dict[word][key]=value.count(word)
+
+r=json.dumps(final_dict)
+fp=open("final.json","w+")
+fp.write(r)			
 
 		
+
 
 
